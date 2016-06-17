03aaffabce7619ddaa3df655ae1a52789f7c69f0
root <root@iZ23m7zqxzcZ.(none)>
2016-06-17 21:25:09 +0800

    update

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from flask import Flask,url_for,redirect,render_template,request
 from werkzeug.utils import secure_filename
-import os,time
+import os,time,re
 
 app = Flask(__name__)
 
@@ -9,21 +9,20 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(),'static')
 
 @app.route("/")
 def index():
-	suimes,others,docs = [],[],[]
-	files = os.listdir('static')
-	for file in files:
-		if file.endswith('.apk'):
-			if file.startswith('Suime'):
-				suimes.append(file)
-			else:
-				others.append(file)
+	newfiles,backups,files = [],[],[]
+	fs = os.listdir('static')
+	for file in fs:
+		if file.endswith('.apk') and not re.search(r"_\d{12}",file):
+			newfiles.append(file)
+		elif file.endswith('.apk'):
+			backups.append(file)
 		else:
-			docs.append(file)
-	suimes = sorted(suimes,key=lambda b: int(b.split('_')[2].split('.')[0]),reverse=True)
-	suimes = readstat(suimes)
-	others = readstat(others)
-	docs = readstat(docs)
-	return render_template("index.html",suimes=suimes,others=others,docs=docs)
+			files.append(file)
+	backups = sorted(backups,key=lambda b: int(b.rsplit('_',1)[1].split('.')[0]),reverse=True)
+	newfiles = readstat(newfiles)
+	backups = readstat(backups)
+	files = readstat(files)
+	return render_template("index.html",newfiles=newfiles,backups=backups,files=files)
 
 def readstat(filelist):
 	flist = []
@@ -46,13 +45,16 @@ def readstat(filelist):
 def upload():
 	try:
 		f = request.files['filecontent']
+		prefix,suffix = f.filename.rsplit(".",1)
+		if f.filename in os.listdir("static"):
+			tmpstr = time.strftime("_%Y%m%d%H%M%S")
+			os.system("mv %s %s" %(f.filename,os.path.join(UPLOAD_FOLDER,"%s%s.%s" %(prefix,tmpstr,suffix))))
 		file = os.path.join(UPLOAD_FOLDER,f.filename)
 		f.save(file)
-	except:
-		pass
+	except Exception as e:
+		print(e)
 	finally:
 		return redirect(url_for(".index"))
-
 
 if __name__ == "__main__":
 	app.run("0.0.0.0",port=8888)
